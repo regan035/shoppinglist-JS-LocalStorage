753be24dd607121e1ec2265cac2cce82b2061cb2
gen <regan035@gmail.com>
2023-11-03 16:36:42 -0500

    delete items

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearBtn = document.getElementById("clear");
 
 const addItem = (e) => {
   e.preventDefault();
@@ -17,7 +18,7 @@ const addItem = (e) => {
   const button = createButton("remove-item btn-link text-red");
   //console.log(button);
   li.appendChild(button);
-  console.log(li);
+  //console.log(li);
 
   // connect to dom
   itemList.appendChild(li);
@@ -40,5 +41,22 @@ createIcon = (classes) => {
   return icon;
 };
 
+const removeItem = (e) => {
+  // console.log(e.target.parentElement.classList);
+  if (e.target.parentElement.classList.contains("remove-item")) {
+    //console.log(e.target.parentElement.parentElement);
+    //e.target.parentElement.remove();
+    e.target.parentElement.parentElement.remove();
+  }
+};
+
+const clearItems = () => {
+  while (itemList.lastChild) {
+    itemList.removeChild(itemList.lastChild);
+  }
+};
+
 // Create event listeners
 itemForm.addEventListener("submit", addItem);
+itemList.addEventListener("click", removeItem);
+clearBtn.addEventListener("click", clearItems);
